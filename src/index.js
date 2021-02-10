ab1a077e2e0d52659b2f48756910df5dcd541d87
Abderrahmane <ar.boulahdour@outlook.com>
2021-02-10 09:34:22 +0000

    third commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import CookiesApp from './Cookies';
-
 import './index.css';
 
-ReactDOM.render(<CookiesApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
